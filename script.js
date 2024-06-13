455c2aa6a0c944a8817c55f2850fe924112229c2
adibadi12 <adrian.mathiassen@gmail.com>
2024-06-13 12:29:13 +0200

    added some styling to the button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello!');
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggleButton');
+    const additionalContent = document.getElementById('additionalContent');
+
+    toggleButton.addEventListener('click',() => {
+        if (additionalContent.style.display === 'none') {
+            additionalContent.style.display = 'block';
+            toggleButton.textContent = 'Show Less';
+        } else {
+            additionalContent.style.display = 'none';
+            toggleButton.textContent = 'Show More';
+        }
+    });
+});
